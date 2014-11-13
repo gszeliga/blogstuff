f3f65d81017cb11646df8d8b643a3f8b0f3eaed3
guillermo <gszeliga@gmail.com>
2014-11-14 00:32:30 +0100

    Finish first implementation of RotatingMap

--- src/main/scala/com/covariantblabbering/rotatingmap/RotatingMap.scala
+++ src/main/scala/com/covariantblabbering/rotatingmap/RotatingMap.scala
@@ -5,21 +5,21 @@ import scala.collection.immutable.HashMap
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.async.Async.async
 
-class RotatingMap[K,V](val numBuckets:Int , val expiredCallback:Option[OnExpiration[K,V]]) {
+class RotatingMap[K, V](val numBuckets: Int, val expiredCallback: Option[OnExpiration[K, V]]) {
 
   assert(numBuckets > 2, "Number of buckets must be greater or equal to 2")
 
-  private var buckets: List[Map[K, V]] = (1 to numBuckets).foldRight(List.empty[Map[K,V]])((_, l) => new HashMap[K,V] :: l)
+  private var _buckets: List[Map[K, V]] = (1 to numBuckets).foldRight(List.empty[Map[K, V]])((_, l) => new HashMap[K, V] :: l)
 
-  def rotate: Map[K,V] = {
+  def rotate: Map[K, V] = {
 
-    def removeLast(source: List[Map[K,V]]) = {
+    def removeLast(source: List[Map[K, V]]) = {
 
       @tailrec
-      def doRemoveLast(tmp:List[Map[K,V]], tail: List[Map[K,V]]): (Map[K,V],List[Map[K,V]]) = {
+      def doRemoveLast(tmp: List[Map[K, V]], tail: List[Map[K, V]]): (Map[K, V], List[Map[K, V]]) = {
 
-        tail match  {
-          case (h :: Nil) => (h,tmp.reverse)
+        tail match {
+          case (h :: Nil) => (h, tmp.reverse)
           case (h :: t) => doRemoveLast(h :: tmp, t)
         }
       }
@@ -28,10 +28,10 @@ class RotatingMap[K,V](val numBuckets:Int , val expiredCallback:Option[OnExpirat
 
     }
 
-    val (dead, remaining) = removeLast(buckets)
+    val (dead, remaining) = removeLast(_buckets)
 
     //Add new bucket at head
-    buckets = new HashMap[K,V] :: remaining
+    _buckets = new HashMap[K, V] :: remaining
 
     //Notify expirations
     async {
@@ -44,5 +44,36 @@ class RotatingMap[K,V](val numBuckets:Int , val expiredCallback:Option[OnExpirat
     dead
 
   }
+
+  def containsKey(key: K): Boolean = _buckets.exists(_.contains(key))
+
+  def get(key: K): Option[V] = _buckets.find(_.contains(key)) flatMap (_.get(key))
+
+  def put(key: K, value: V) = {
+    val head = _buckets.head + (key -> value)
+    val tail = _buckets.tail.foldLeft(List.empty[Map[K, V]]) { (l, m) => (m - key) :: l}
+
+    _buckets = head :: tail
+  }
+
+  def remove(key: K): Option[V] = {
+    val (removed, remaining) = _buckets.foldLeft((Option.empty[V], List.empty[Map[K, V]])) { (acc, m) =>
+
+      acc match {
+        case (r, l) => {
+          if (m.contains(key)) {
+            (Some(m(key)), m - key :: l)
+          }
+          else (r, m :: l)
+        }
+      }
+    }
+
+    _buckets = remaining
+
+    removed
+  }
+
+  def size: Int = _buckets.foldLeft(0){ _ + _.size}
 
 }
